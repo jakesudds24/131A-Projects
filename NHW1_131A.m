5b680d243bdaad48ce19e8cdbc6b81bb585d0c59
jakesudds24 <130803800+jakesudds24@users.noreply.github.com>
2025-10-22 12:57:31 -0700

    Add files via upload

--- NHW1_131A.m
+++ NHW1_131A.m
@@ -23,6 +23,10 @@ Tinf = 180+273;%K
 h = 2000;     %W/m^2/K
 qdot = 1.4e6;  %W/m^3
 q_in = -2000;  %W/m^2
+%Known temperature at the interface of middle and metal
+T2 = 182.1 + 273;
+%Known temperature at the metal and convection interface
+T3 = 181.93 + 273;
 %Convection temp and coeff, volumetric heat gen in tube, and heat flux to
 %inner portion of the tube all in SI units.
 
@@ -31,41 +35,43 @@ q_in = -2000;  %W/m^2
 %the metal is taken to be T2
 
 %Defining number of nodes
-n_mid = 10;
-%Known temperature at the interface of middle and metal
-T2 = 182.1 + 273;
-%Known temperature at the metal and convection interface
-T3 = 181.93 + 273;
+n_mid = [5,10,100,500,1000];
+
+err = zeros(5,1000);
+L2Err = zeros(1,5);
+LinfErr = zeros(1,5);
+
+for j = 1:1:5
 %Distance between nodes
-dr = (r2-r1)/n_mid;
+dr = (r2-r1)/n_mid(j);
 %Creating nodes for metal
 n_met = (r3-r2)/dr;
 %Creating a vector of each node location
-r = linspace(r1,r2,n_mid);
+r = linspace(r1,r2,n_mid(j));
 
 %%
 %For interior nodes,
-n_unk = n_mid - 2;
+n_unk = n_mid(j) - 2;
 
 %Simplifying variable
 S = -qdot/k*dr^2;
 
 %Creating A and C matrices for solving equations, [A][T]=[C]
 %Check the hand work to understand where these come from
-A = zeros(n_mid);
-C = zeros(n_mid,1);
+A = zeros(n_mid(j));
+C = zeros(n_mid(j),1);
 
 %Creating the A matrix for interior nodes, the one multiplying the temperatures
-for i = 2:n_mid - 1
+for i = 2:n_mid(j) - 1
     A(i,i) = -2;
     A(i,i-1) = (1 - dr / (2*r(i)) );
     A(i,i+1) = (1 + dr / (2*r(i)) );
 end
 
 %Creating the C matrix, the right hand side of the equation
-for i = 1:n_mid
+for i = 1:n_mid(j)
     if i == 1, C(i) = -q_in*dr/k;
-    elseif i == n_mid, C(i) = T2;
+    elseif i == n_mid(j), C(i) = T2;
     else, C(i) = S;
     end
 end
@@ -73,19 +79,10 @@ end
 %Adding in boundary conditions
 A(1,1) = -1;
 A(1,2) = 1;
-A(n_mid,n_mid) = 1;
+A(n_mid(j),n_mid(j)) = 1;
 
 %Calculating Temperature
 T = A\C;  %This looks backwards but is how matlab works
-
-%Now find the temperature in the metal given the temperature and convective
-%boundary condition on the outside
-
-
-
-
-
-
 
 %Plotting the temperature over the radius
 figure(1)
@@ -98,26 +95,39 @@ ylabel("Temperature (^oC)")
 
 %Temperature distribution in the mid portion
 %Creating radius vector
-ra = linspace(r1,r2,n_mid);
+ra = linspace(r1,r2,n_mid(j));
 %Constants found by hand
 C1 = -r1/k * (q_in - qdot/2*r1);
 C2 = T2 + qdot/4/k * r2^2 - C1*log(r2);
 %Temperature distribution from heat equation
 Ta = -qdot/4/k*ra.^2 + C1.*log(ra) + C2;
 
-%Temperature distribution in the metal sheet
-%Creating radius vector
-rm = linspace(r2,r3,n_met);
-%Constants
-C3 = -r3*h*(T3-Tinf);
-C4 = T2 - C3/kg*log(r2);
-%Temperature distribution
-Tam = C3/kg.*log(rm)+C4;
-
 figure(1)
 hold on
 plot(ra*10^3,Ta-273)
-plot(rm*10^3,Tam-273)
 legend('Numerical Solution','Analytical Solution')
 title('Numerical and Analytical Temperature Distributions in the Mid Cylinder')
 hold off
+
+
+%%
+%Calculating the error
+
+err(j,1:n_mid(j))     = T - Ta(:,1);
+LinfErr(j)   = max(abs(err(j,:)));
+L2Err(j)     = norm(err(j,1:n_mid(j)))/sqrt(numel(err(j,1:n_mid(j))));
+end
+
+%Plot the least squares error for each number of cells used
+figure(2)
+hold on
+plot(n_mid,L2Err,'ro')
+plot(n_mid,L2Err,'r')
+hold off
+title('Least Squares Error Plotted Against Number of Cells Used')
+xlabel("Number of Cells")
+ylabel("Least Squares Error")
+
+
+
+
